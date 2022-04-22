3a47f035870fab0c6726b8a6d0058433530546d0
Daniel Schemp <dschemp@mailbox.org>
2022-04-22 18:51:50 +0200

    added first structure

--- cmd/prntserve/main.go
+++ cmd/prntserve/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	internal "github.com/dschemp/go-prntserve/internal"
+	"github.com/dschemp/go-prntserve/internal/cmd"
+	"os"
+)
+
 func main() {
-	println("Welcome to prntserve!")
+	cmd.ParseArguments()
+	fmt.Println("Welcome to prntserve!")
+
+	if cmd.ShowVersion() {
+		// TODO: Show version
+		fmt.Printf("Version: %s\n", "unknown")
+		os.Exit(0)
+	}
+
+	// Start server
+	internal.StartServer()
 }
